7d0468389cffc978b7feba846eb7bd84670b6be9
yash mantri <mantriyash2025@gmail.com>
2020-03-24 00:20:01 +0530

    Updates

---
+++ precache-manifest.6069fb77503b67d38d4e0644d4d6786a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa2d7a436faa59d40adfcb399d1836a",
+    "url": "/yashmantri/index.html"
+  },
+  {
+    "revision": "bfb393a87d6621cfeb41",
+    "url": "/yashmantri/static/css/2.1cc03777.chunk.css"
+  },
+  {
+    "revision": "96a88993397de7e3afb0",
+    "url": "/yashmantri/static/css/main.eaaa206d.chunk.css"
+  },
+  {
+    "revision": "bfb393a87d6621cfeb41",
+    "url": "/yashmantri/static/js/2.9aec3359.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/yashmantri/static/js/2.9aec3359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a88993397de7e3afb0",
+    "url": "/yashmantri/static/js/main.d294da4d.chunk.js"
+  },
+  {
+    "revision": "337fc19b1c7d946abeaa",
+    "url": "/yashmantri/static/js/runtime-main.5de778c5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/yashmantri/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/yashmantri/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/yashmantri/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/yashmantri/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/yashmantri/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
