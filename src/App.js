f004089cf48c2e9fe2848e5310fe577b2fe2469c
yash mantri <mantriyash2025@gmail.com>
2020-03-24 00:29:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Grid, Cell} from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 
